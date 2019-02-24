f793d1888b25fce4019992d6d7034dbc951b733c
alex <alex@delifer.ca>
2019-02-24 17:14:22 -0500

    cleanup

--- doom/install.sh
+++ doom/install.sh
@@ -9,29 +9,29 @@ fi
 # install vim package
 if [[ $(uname) == 'Linux' ]]; then
 
-	if ! command -v nvim >/dev/null 2>&1; then
+	if ! command -v emacs >/dev/null 2>&1; then
 		if [[ $(uname) =~ (.*)Debian(.*) ]]; then
-			$SUDO apt-get install -y neovim
+			$SUDO apt-get install -y emacs
 		elif [[ $(uname) =~ (.*)Ubuntu(.*) ]]; then
-			$SUDO apt-get install -y neovim
+			$SUDO apt-get install -y emacs
 		elif [[ $(uname) =~ (.*)Linux(.*) ]]; then
-			$SUDO pacman -S neovim
+			$SUDO pacman -S emacs
 		fi
 	fi
 
 elif [[ $(uname) == 'Darwin' ]] && ! brew list | grep -q vim; then
-	brew install vim
+	brew install emacs
 fi
 
 # retrieve the Vundle submodule
-git submodule update --init nvim/.config/nvim/autoload
+git submodule update --init doom/.emacs.d
 
 # install vim plugins with Vundle
 if [[ $? -eq 0 ]]; then
 	# if either stdin or stdout is missing, redirect output
 	if [[ ! -t 0 || ! -t 1 ]]; then
-		nvim +PlugUpdate +qall &>/dev/null
+        y | doom/.emacs.d/bin/doom quickstart &>/dev/null
 	else
-		nvim +PlugUpdate +qall
+        doom/.emacs.d/bin/doom quickstart
 	fi
 fi
---
+++ git/install.sh
@@ -0,0 +1,22 @@
+#! /bin/bash
+
+if ! command -v git >/dev/null 2>&1; then
+
+        # install git package
+        if [[ $(uname) == 'Darwin' ]]; then
+                if ! command -v brew >/dev/null 2>&1; then
+                        echo 'Run ./install.sh osx first to bootstrap OSX with Homebrew'
+                        exit 3
+                fi
+                brew install git
+
+        elif [[ $(uname) == 'Linux' ]]; then
+                sudo pacman -S git
+        fi
+
+fi
+
+# create bin directory in $HOME before stow symlinks into it
+if [[ ! -d $HOME/bin ]]; then
+        mkdir $HOME/bin
+fi
