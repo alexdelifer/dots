fa589ae0b5f90344d6d59d04a6f7640151cd1222
alex <alex@delifer.ca>
2019-02-24 18:09:36 -0500

    stuff

---
+++ tmux/install.sh
@@ -0,0 +1,28 @@
+#! /bin/bash
+
+if [[ $(uname) == 'Darwin' ]]; then
+	# I don't use tmux on OSX
+	return
+fi
+
+if [[ $(id -u) -gt 0 ]]; then
+	SUDO='sudo'
+else
+	SUDO=''
+fi
+
+if ! command -v tmux >/dev/null 2>&1; then
+
+	# install tmux package
+	if [[ $(uname) == 'Linux' ]]; then
+		if [[ $(id -u) -gt 0 ]]; then
+			$SUDO pacman -S tmux
+		else
+			pacman -S tmux
+		fi
+	fi
+fi
+
+tmux -f tmux/.tmux.conf new-session \
+	"tmux run-shell tmux/.tmux/plugins/tpm/bindings/install_plugins" \
+	&& echo "tmux installed."
