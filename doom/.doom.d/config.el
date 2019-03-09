bf296c01ad921f757fbd02b0bce33427641fefe8
alex <alex@delifer.ca>
2019-03-08 21:40:46 -0500

    migrate to exwm

--- doom/.doom.d/config.el
+++ doom/.doom.d/config.el
@@ -3,3 +3,17 @@
 ;; Place your private configuration here
 (setq doom-font (font-spec :family "SF Mono" :size 14)
       doom-big-font (font-spec :family "SF Mono" :size 19))
+
+;; EXWM
+(require 'exwm)
+(require 'exwm-config)
+
+;; Set the initial workspace number
+(setq exwm-workspace-number 4)
+(setq exwm-workspace-show-all-buffers t)
+(setq exwm-layout-show-all-buffers t)
+
+
+(exwm-config-default)
+(require 'exwm-systemtray)
+(exwm-systemtray-enable)
--- doom/.doom.d/packages.el
+++ doom/.doom.d/packages.el
@@ -5,3 +5,7 @@
 ;; (package! some-package)
 ;; (package! another-package :recipe (:fetcher github :repo "username/repo"))
 ;; (package! builtin-package :disable t)
+;;; Code:
+(package! exwm)
+(package! helm-exwm)
+(package! fancy-battery)
