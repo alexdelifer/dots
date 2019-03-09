62a3dba580da21c78d2035ca6f4bd6f38f5dbad7
alex <alex@delifer.ca>
2019-03-08 22:05:13 -0500

    Kill spacemacs buffer on load

--- spacemacs/.spacemacs.d/init.el
+++ spacemacs/.spacemacs.d/init.el
@@ -458,9 +458,12 @@ This function is called at the very end of Spacemacs startup, after layer
 configuration.
 Put your configuration code here, except for variables that should be set
 before packages are loaded."
+  ;; Cleanup stupid spacemacs buffer
+  (kill-buffer "*spacemacs*")
   (fancy-battery-mode)
   (require 'exwm-systemtray)
   (exwm-systemtray-enable)
+  (setq exwm-workspace-number 4)
   )
 
 ;; Do not write anything past this comment. This is where Emacs will
