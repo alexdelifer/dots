0e3c9e5c405931087a2563597506a2b2461ba9a4
Alexandre Delifer <alex@delifer.ca>
2021-12-18 23:02:58 -0500

    moved perf kernels to gaming

--- aconfmgr/.config/aconfmgr/30-gaming.sh
+++ aconfmgr/.config/aconfmgr/30-gaming.sh
@@ -6,6 +6,14 @@
 
 CheckConfig gaming || return 0
 
+# kernels
+AddPackage linux-xanmod-tt # The Linux kernel and modules with Xanmod patches
+AddPackage linux-xanmod-tt-headers # Headers and scripts for building modules for the Linux Xanmod. Development branch with the Task Type CPU Scheduler by Hamad Al Marri kernel
+AddPackage linux-zen # The Linux ZEN kernel and modules
+AddPackage linux-zen-headers # Headers and scripts for building modules for the Linux ZEN kernel
+CopyFile /etc/mkinitcpio.d/linux-xanmod-tt.preset
+CopyFile /etc/mkinitcpio.d/linux-zen.preset
+
 # apps
 AddPackage steam # Valve's digital software delivery system
 AddPackage lutris # Open Gaming Platform
